3c400d5f7c74dd6ca7aea31d4ae73f9c7a60c5d1
siddu <siddukankanawadi6@gmail.com>
2023-03-23 15:40:06 +0530

    delete

--- ex5/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-bucket-96"
-    key    = "terraform/backend"
-    region = "us-east-1"
-  }
-}
--- ex6/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-bucket-96"
-    key    = "terraform/backend_exercise6"
-    region = "us-east-1"
-  }
-}
